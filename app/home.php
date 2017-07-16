4ce0e242001ad94a579fe898c423f6e52eca36a3
Amir Mehdi Savand <amir@savandbros.com>
2017-07-17 00:26:31 +0300

    Add cover to home

---
+++ app/home.php
@@ -0,0 +1,7 @@
+<!-- Cover -->
+<div class="container-fluid cover bg-header">
+  <div class="container">
+    <h1 class="t-5">Kloud51</h1>
+    <h1>Web Hosting, Blazing Fast SSD VPS, Domain Names and SSL Certificate.</h1>
+  </div>
+</div>
