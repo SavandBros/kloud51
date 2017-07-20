7a6a1709eaefe44d77abd5074408e102d2268a99
Arsalan Savand <arsalan@savandbros.com>
2017-07-20 15:49:07 +0400

    Add why Kloud51 section to main page

--- app/home.php
+++ app/home.php
@@ -58,3 +58,45 @@ $plans = $db->select("SELECT * FROM `plans`");
     </div>
   </div>
 </div>
+<!-- Why Kloud51? -->
+<div class="container-fluid bg-1" id="why">
+  <div class="container s-pad-y-hard">
+    <!-- Title -->
+    <h1 bold>
+      <a href="#why">Why Kloud51?</a>
+    </h1>
+    <!-- Reasons -->
+    <div class="row s-mar-top text-center">
+      <div class="col-md-4">
+        <i class="fa fa-check-circle"></i>
+        <h4>QUALITY HOSTING</h4>
+        <p>With a selection of server hardware and infrastructure sourced from globally-reputable providers, we guarantee 99.9% uptime 24 hours a day, 7 days a week.</p>
+      </div>
+      <div class="col-md-4">
+        <i class="fa fa-shield"></i>
+        <h4>COMPLETE PROTECTION</h4>
+        <p>We provide anti-DDoS, Firewalls, and Antivirus to protect your data, with options for RAID-1 or RAID-10 redundancy for that extra-sensitive information.</p>
+      </div>
+      <div class="col-md-4">
+        <i class="fa fa-comments"></i>
+        <h4>EXCELLENT SUPPORT</h4>
+        <p>Having issues? We are dedicated to swiftly resolving issues and will assign a technician you help you every step of the way for techniclal help.</p>
+      </div>
+      <div class="col-md-4">
+        <i class="fa fa-code"></i>
+        <h4>CONSTANT DEVELOPMENT</h4>
+        <p>We are always improving and developing to add more features and better services. Missing something? Let us know so we can work on it!</p>
+      </div>
+      <div class="col-md-4">
+        <i class="fa fa-database"></i>
+        <h4>SOLID INFRASTRUCTURE</h4>
+        <p>Kloud51 servers are being maintained round-the-clock by our technical staff and are connected to a global network with over 3 Tbps of total bandwidth.</p>
+      </div>
+      <div class="col-md-4">
+        <i class="fa fa-bolt"></i>
+        <h4>LIGHTNING PERFORMANCE</h4>
+        <p>All of our servers feature high-performance hardware with optimized configurations, and are constantly being monitored by our developers and admins.</p>
+      </div>
+    </div>
+  </div>
+</div>
