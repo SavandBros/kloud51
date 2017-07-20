f975f453cd3886fb61c77cf56d0ca90156e66099
Amir Mehdi Savand <amir@savandbros.com>
2017-07-20 16:58:33 +0400

    Design why section

--- app/home.php
+++ app/home.php
@@ -80,42 +80,95 @@ $plans = $db->select("SELECT * FROM `plans`");
   </div>
 </div>
 
-<!-- Why Kloud51? -->
 <div class="container-fluid bg-1" id="why">
   <div class="container s-pad-y-hard">
     <!-- Title -->
     <h1 class="header"><a href="#why">Why Kloud51?</a></h1>
     <!-- Reasons -->
-    <div class="row s-mar-top text-center">
-      <div class="col-md-4">
-        <i class="fa fa-check-circle"></i>
-        <h4>QUALITY HOSTING</h4>
-        <p>With a selection of server hardware and infrastructure sourced from globally-reputable providers, we guarantee 99.9% uptime 24 hours a day, 7 days a week.</p>
+    <div class="row why-list">
+      <div class="col-lg-6">
+        <div class="row">
+          <div class="col-sm-3">
+            <i class="fa fa-check-circle" style="color: #2ecc71"></i>
+          </div>
+          <div class="col-sm-9">
+            <h3>Quality Hosting</h3>
+            <p>
+              With a selection of server hardware and infrastructure sourced from
+              globally-reputable providers, we guarantee 99.9% uptime 24 hours a day, 7 days a week.
+            </p>
+          </div>
+        </div>
       </div>
-      <div class="col-md-4">
-        <i class="fa fa-shield"></i>
-        <h4>COMPLETE PROTECTION</h4>
-        <p>We provide anti-DDoS, Firewalls, and Antivirus to protect your data, with options for RAID-1 or RAID-10 redundancy for that extra-sensitive information.</p>
+      <div class="col-lg-6">
+        <div class="row">
+          <div class="col-sm-3">
+            <i class="fa fa-shield" style="color: tomato"></i>
+          </div>
+          <div class="col-sm-9">
+            <h3>Complete Protection</h3>
+            <p>
+              We provide anti-DDoS, Firewalls, and Antivirus to protect your data,
+              with options for RAID-1 or RAID-10 redundancy for that extra-sensitive information.
+            </p>
+          </div>
+        </div>
       </div>
-      <div class="col-md-4">
-        <i class="fa fa-comments"></i>
-        <h4>EXCELLENT SUPPORT</h4>
-        <p>Having issues? We are dedicated to swiftly resolving issues and will assign a technician you help you every step of the way for techniclal help.</p>
+      <div class="col-lg-6">
+        <div class="row">
+          <div class="col-sm-3">
+            <i class="fa fa-comments" style="color: #9b59b6"></i>
+          </div>
+          <div class="col-sm-9">
+            <h3>Excellent Support</h3>
+            <p>
+              Having issues? We are dedicated to swiftly resolving issues and will assign
+              a technician you help you every step of the way for techniclal help.
+            </p>
+          </div>
+        </div>
       </div>
-      <div class="col-md-4">
-        <i class="fa fa-code"></i>
-        <h4>CONSTANT DEVELOPMENT</h4>
-        <p>We are always improving and developing to add more features and better services. Missing something? Let us know so we can work on it!</p>
+      <div class="col-lg-6">
+        <div class="row">
+          <div class="col-sm-3">
+            <i class="fa fa-code" style="color: #1abc9c"></i>
+          </div>
+          <div class="col-sm-9">
+            <h3>Constant Development</h3>
+            <p>
+              We are always improving and developing to add more features and better services.
+              Missing something? Let us know so we can work on it!
+            </p>
+          </div>
+        </div>
       </div>
-      <div class="col-md-4">
-        <i class="fa fa-database"></i>
-        <h4>SOLID INFRASTRUCTURE</h4>
-        <p>Kloud51 servers are being maintained round-the-clock by our technical staff and are connected to a global network with over 3 Tbps of total bandwidth.</p>
+      <div class="col-lg-6">
+        <div class="row">
+          <div class="col-sm-3">
+            <i class="fa fa-database" style="color: #e67e22"></i>
+          </div>
+          <div class="col-sm-9">
+            <h3>Solid Infrastructure</h3>
+            <p>
+              Kloud51 servers are being maintained round-the-clock by our technical staff
+              and are connected to a global network with over 3 Tbps of total bandwidth.
+            </p>
+          </div>
+        </div>
       </div>
-      <div class="col-md-4">
-        <i class="fa fa-bolt"></i>
-        <h4>LIGHTNING PERFORMANCE</h4>
-        <p>All of our servers feature high-performance hardware with optimized configurations, and are constantly being monitored by our developers and admins.</p>
+      <div class="col-lg-6">
+        <div class="row">
+          <div class="col-sm-3">
+            <i class="fa fa-bolt" style="color: #3498db"></i>
+          </div>
+          <div class="col-sm-9">
+            <h3>Lightning Performance</h3>
+            <p>
+              All of our servers feature high-performance hardware with optimized configurations,
+              and are constantly being monitored by our developers and admins.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
