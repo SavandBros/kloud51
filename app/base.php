cb9fa8fa422239bab7138bb660ca39c9e9897742
Amir Mehdi Savand <amir@savandbros.com>
2017-08-17 21:25:39 +0400

    Update footer structure

--- app/base.php
+++ app/base.php
@@ -50,219 +50,153 @@ require "include/route.php";
   <div class="page">
     <?php include $base->include; ?>
   </div>
-  <div class="container-fluid s-pad-no">
-    <div class="social row">
-      <a class="col-md-2 col-xs-4" target="_blank" href="http://forum.kloud51.com" title="Kloud51 Forum">
-        <i class="fa fa-cloud"></i>
-      </a>                    
-      <a class="col-md-2 col-xs-4" target="_blank" href="https://twitter.com/Kloud51" style="background: #0084b4">
-        <i class="fa fa-twitter"></i>
-      </a>
-      <a class="col-md-2 col-xs-4" target="_blank" href="https://instagram.com/Kloud51/" style="background: #3f729b">
-        <i class="fa fa-instagram"></i>
-      </a>
-      <a class="col-md-2 col-xs-4" target="_blank" href="https://www.facebook.com/savand.bros" style="background: #3b5998">
-        <i class="fa fa-facebook"></i>
-      </a>
-      <a class="col-md-2 col-xs-4" target="_blank" href="https://plus.google.com/+Kloud51Host" style="background: #dc4e41">
-        <i class="fa fa-google-plus"></i>
-      </a>
-      <a class="col-md-2 col-xs-4" target="_blank" href="https://www.linkedin.com/company/savand.bros" style="background: #0077b5">
-        <i class="fa fa-linkedin"></i>
-      </a>
-    </div>
-  </div>
-  <div class="container-fluid" id="footer">
-    <div class="container">
+
+  <!-- Footer -->
+  <div class="footer">
+
+    <!-- Social -->
+    <div class="social container-fluid s-pad-no">
       <div class="row">
-        <div class="col-md-3">
-          <h4>
-            <i class="fa fa-cloud fa-fw"></i> Comapny
-          </h4>
-          <hr>
-          <ul class="list-unstyled">
-            <li>
-              <a href="http://forum.kloud51.com/t/what-s-up" target="_self">
-                <i class="fa fa-comment-o"></i> What's Up
-              </a>
-            </li>
-            <li>
-              <a href="http://forum.kloud51.com/" target="_self">
-                <i class="fa fa-comments-o"></i> Community
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/announcements.php" target="_self">
-                <i class="fa fa-bullhorn"></i> Announcements
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/about" target="_self">
-                <i class="fa fa-info-circle"></i> About
-              </a>
-            </li>
-            <li class="dropdown">
+        <a class="col-md-2 col-xs-4" target="_blank" href="https://forum.kloud51.com" title="Kloud51 Forum">
+          <i class="fa fa-cloud"></i>
+        </a>                    
+        <a class="col-md-2 col-xs-4" target="_blank" href="https://twitter.com/Kloud51" style="background: #0084b4">
+          <i class="fa fa-twitter"></i>
+        </a>
+        <a class="col-md-2 col-xs-4" target="_blank" href="https://instagram.com/Kloud51/" style="background: #3f729b">
+          <i class="fa fa-instagram"></i>
+        </a>
+        <a class="col-md-2 col-xs-4" target="_blank" href="https://www.facebook.com/savand.bros" style="background: #3b5998">
+          <i class="fa fa-facebook"></i>
+        </a>
+        <a class="col-md-2 col-xs-4" target="_blank" href="https://plus.google.com/+Kloud51Host" style="background: #dc4e41">
+          <i class="fa fa-google-plus"></i>
+        </a>
+        <a class="col-md-2 col-xs-4" target="_blank" href="https://www.linkedin.com/company/savand.bros" style="background: #0077b5">
+          <i class="fa fa-linkedin"></i>
+        </a>
+      </div>
+    </div>
+
+    <!-- Sections -->
+    <div class="section container-fluid">
+      <div class="container">
+        <div class="row">
+          <!-- Company -->
+          <div class="col-md-3">
+            <!-- Title -->
+            <h4><i class="fa fa-cloud fa-fw"></i> Comapny</h4>
+            <!-- Links -->
+            <div class="links">
+              <!-- What's up -->
+              <a href="https://forum.kloud51.com/t/what-s-up"><i class="fa fa-comment-o"></i> What's Up</a>
+              <!-- Community -->
+              <a href="https://forum.kloud51.com/"><i class="fa fa-comments-o"></i> Community</a>
+              <!-- Announcements -->
+              <a href="//kloud51.com/announcements.php"><i class="fa fa-bullhorn"></i> Announcements</a>
+              <!-- About -->
+              <a href="//kloud51.com/about"><i class="fa fa-info-circle"></i> About</a>
+              <!-- Legal -->
               <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                 <i class="fa fa-gavel"></i> Legal <b class="caret"></b>
               </a>
+              <!-- Legal (dropdown) -->
               <ul class="dropdown-menu">
-                <li>
-                  <a href="//kloud51.com/tos" target="_self">
-                    <i class="fa fa-gavel"></i> Terms of Service
-                  </a>
-                </li>
-                <li>
-                  <a href="//kloud51.com/acceptable-use-policy" target="_self">
-                    <i class="fa fa-gavel"></i> Acceptable Use Policy
-                  </a>
-                </li>
-                <li>
-                  <a href="//kloud51.com/refunds" target="_self">
-                    <i class="fa fa-gavel"></i> Refund Policy
-                  </a>
-                </li>
-                <li>
-                  <a href="//kloud51.com/privacy" target="_self">
-                    <i class="fa fa-gavel"></i> Privacy Policy
-                  </a>
-                </li>
+                <li><a href="//kloud51.com/tos">Terms of Service</a></li>
+                <li><a href="//kloud51.com/acceptable-use-policy">Acceptable Use Policy</a></li>
+                <li><a href="//kloud51.com/refunds">Refund Policy</a></li>
+                <li><a href="//kloud51.com/privacy">Privacy Policy</a></li>
               </ul>
-            </li>
-            <li>
-              <a href="http://badge.kloud51.com" target="_self">
-                <i class="fa fa-fw fa-tags"></i> Badges
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-3">
-          <h4>
-            <i class="fa fa-cubes fa-fw"></i> Services
-          </h4>
-          <hr>
-          <ul class="list-unstyled">
-            <li>
-              <a href="//kloud51.com/cart.php?gid=1" target="_self">
+              <!-- Badges -->
+              <a href="http://badge.kloud51.com"><i class="fa fa-fw fa-tags"></i> Badges</a>
+            </div>
+          </div>
+          <!-- Services -->
+          <div class="col-md-3">
+            <!-- Title -->
+            <h4><i class="fa fa-cubes fa-fw"></i> Services</h4>
+            <!-- Links -->
+            <div class="links">
+              <!-- Web Hosting (cPanel) -->
+              <a href="https://billing.kloud51.com/cart.php?gid=1">
                 <i class="fa fa-server"></i> Web Hosting (cPanel)
               </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/cart.php?gid=4" target="_self">
+              <!-- SSD -->
+              <a href="https://billing.kloud51.com/cart.php?gid=4">
                 <i class="fa fa-spinner"></i> SSD Virtual Private Server (VPS)
               </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/cart.php?gid=3" target="_self">
+              <!-- Reseller -->
+              <a href="https://billing.kloud51.com/cart.php?gid=3">
                 <i class="fa fa-repeat"></i> Reseller Linux Web Hosting
               </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/cart.php?gid=2" target="_self">
+              <!-- SSL -->
+              <a href="https://billing.kloud51.com/cart.php?gid=2">
                 <i class="fa fa-shield"></i> SSL Certificates
               </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/cart.php?gid=5" target="_self">
-                <i class="fa fa-file-text-o"></i> Software & Server Licenses
+              <!-- Software & Server Licenses -->
+              <a href="https://billing.kloud51.com/cart.php?gid=5">
+                <i class="fa fa-file-text-o"></i> Software &amp; Server Licenses
               </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/affiliate-army-program" target="_self">
+              <!-- Affiliate Army -->
+              <a href="/affiliate-army-program">
                 <i class="fa fa-rebel"></i> Affiliate Army
               </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/sponsorship" target="_self">
+              <!-- Sponsorship -->
+              <a href="/sponsorship">
                 <i class="fa fa-users"></i> Sponsorship
               </a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-3">
-          <h4>
-            <i class="fa fa-phone-square"></i> SUPPORT
-          </h4>
-          <hr>
-          <ul class="list-unstyled">
-            <li>
-              <a href="https://vmanager.kloud51.com" target="_self">
-                <i class="fa fa-fw fa-server"></i> VM Manager
-              </a>
-            </li>
-            <li>
-              <a href="http://forum.kloud51.com/t/feature-requests" target="_self">
-                <i class="fa fa-fw fa-star-half-o"></i> Feature Requests
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/knowledgebase.php" target="_self">
-                <i class="fa fa-book"></i> Knowledgebase
-              </a>
-            </li>
-            <li>
-              <a href="https://kloud51.com/serverstatus.php" target="_self">
-                <i class="fa fa-fw fa-info-circle"></i> Server Status
-              </a>
-            </li>
-            <li>
-              <a href="https://kloud51.com/contact.php" target="_self">
-                <i class="fa fa-fw fa-comments"></i> Contact Us
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-3">
-          <h4>
-            <i class="fa fa-fw fa-globe"></i> HOSTING
-          </h4>
-          <hr>
-          <ul class="list-unstyled">
-            <li>
-              <a href="//kloud51.com/ssd-vps-hosting" target="_self">
-                <i class="fa fa-server fa-fw"></i> SSD VPS Hosting
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/wordpress-hosting" target="_self">
-                <i class="fa fa-wordpress fa-fw"></i> WordPress Hosting
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/joomla-hosting" target="_self">
-                <i class="fa fa-fw fa-joomla"></i> Joomla Hosting
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/whmcs-hosting" target="_self">
-                <i class="fa fa-cogs"></i> WHMCS Hosting
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/mybb-hosting" target="_self">
-                <i class="fa fa-fw fa-comments"></i> MyBB Hosting
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/MariaDB-hosting" target="_self">
-                <i class="fa fa-fw fa-database"></i> MariaDB Hosting
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/bbPress-hosting" target="_self">
-                <i class="fa fa-fw fa-database"></i> bbPress Hosting
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/softaculous-hosting" target="_self">
-                <i class="fa fa-fw fa-cube"></i> Softaculous
-              </a>
-            </li>
-            <li>
-              <a href="//kloud51.com/prestashop-hosting" target="_self">
-                <i class="fa fa-fw fa-shopping-cart"></i> PrestaShop
-              </a>
-            </li>
-          </ul>
+            </div>
+          </div>
+          <!-- Support -->
+          <div class="col-md-3">
+            <h4><i class="fa fa-phone-square"></i> Support</h4>
+                <a href="https://vmanager.kloud51.com">
+                  <i class="fa fa-fw fa-server"></i> VM Manager
+                </a>
+                <a href="https://forum.kloud51.com/t/feature-requests">
+                  <i class="fa fa-fw fa-star-half-o"></i> Feature Requests
+                </a>
+                <a href="//kloud51.com/knowledgebase.php">
+                  <i class="fa fa-book"></i> Knowledgebase
+                </a>
+                <a href="https://kloud51.com/serverstatus.php">
+                  <i class="fa fa-fw fa-info-circle"></i> Server Status
+                </a>
+                <a href="https://kloud51.com/contact.php">
+                  <i class="fa fa-fw fa-comments"></i> Contact Us
+                </a>
+          </div>
+          <!-- Hosting -->
+          <div class="col-md-3">
+            <h4><i class="fa fa-fw fa-globe"></i> Hosting</h4>
+                <a href="//kloud51.com/ssd-vps-hosting">
+                  <i class="fa fa-server fa-fw"></i> SSD VPS Hosting
+                </a>
+                <a href="//kloud51.com/wordpress-hosting">
+                  <i class="fa fa-wordpress fa-fw"></i> WordPress Hosting
+                </a>
+                <a href="//kloud51.com/joomla-hosting">
+                  <i class="fa fa-fw fa-joomla"></i> Joomla Hosting
+                </a>
+                <a href="//kloud51.com/whmcs-hosting">
+                  <i class="fa fa-cogs"></i> WHMCS Hosting
+                </a>
+                <a href="//kloud51.com/mybb-hosting">
+                  <i class="fa fa-fw fa-comments"></i> MyBB Hosting
+                </a>
+                <a href="//kloud51.com/MariaDB-hosting">
+                  <i class="fa fa-fw fa-database"></i> MariaDB Hosting
+                </a>
+                <a href="//kloud51.com/bbPress-hosting">
+                  <i class="fa fa-fw fa-database"></i> bbPress Hosting
+                </a>
+                <a href="//kloud51.com/softaculous-hosting">
+                  <i class="fa fa-fw fa-cube"></i> Softaculous
+                </a>
+                <a href="//kloud51.com/prestashop-hosting">
+                  <i class="fa fa-fw fa-shopping-cart"></i> PrestaShop
+                </a>
+          </div>
         </div>
       </div>
     </div>
