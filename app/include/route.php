1308f633fe1bd98439646845d3d4ab84b2b2e2cb
Arsalan Savand <arsalan@savandbros.com>
2017-08-20 18:30:11 +0400

    Add terms of service route

--- app/include/route.php
+++ app/include/route.php
@@ -105,6 +105,7 @@ class Route {
 Route::add("home");
 Route::add("affiliate", "/program/affiliate-army", "Afflicate Army Program", "Program", true);
 Route::add("sponsorship", "/program/sponsorship", "Sponsorship", "Program", true);
+Route::add("tos", "/tos", "tos", null, true);
 
 // Default route
 $base = Route::find("home");
