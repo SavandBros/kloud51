e9593fb20b46aec99adf9358310eaf4cb3ad62d6
Arsalan Savand <arsalan@savandbros.com>
2017-08-20 19:49:30 +0400

    Link terms of service page

--- app/base.php
+++ app/base.php
@@ -127,7 +127,7 @@ require "include/route.php";
               </a>
               <!-- Legal (dropdown) -->
               <ul class="dropdown-menu">
-                <li><a href="//kloud51.com/tos">Terms of Service</a></li>
+                <li><a href="/tos">Terms of Service</a></li>
                 <li><a href="//kloud51.com/acceptable-use-policy">Acceptable Use Policy</a></li>
                 <li><a href="//kloud51.com/refunds">Refund Policy</a></li>
                 <li><a href="//kloud51.com/privacy">Privacy Policy</a></li>
