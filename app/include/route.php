0b3d45b7318eb8e5a810111873537ec73c4c0d4e
Arsalan Savand <arsalan@savandbros.com>
2017-08-21 18:10:19 +0400

    Add myBB hosting to route

--- app/include/route.php
+++ app/include/route.php
@@ -105,6 +105,7 @@ class Route {
 Route::add("home");
 Route::add("affiliate", "/program/affiliate-army", "Afflicate Army Program", "Program", true);
 Route::add("sponsorship", "/program/sponsorship", "Sponsorship", "Program", true);
+Route::add("mybb", "/hostings/mybb", "MyBB - ", "Hostings", true);
 
 // Default route
 $base = Route::find("home");
