25ea3da4a3b32212acef6d1a6cd26acf695dc089
Arsalan Savand <arsalan@savandbros.com>
2017-08-22 23:12:26 +0400

    Include grid js in base php

--- app/base.php
+++ app/base.php
@@ -269,6 +269,7 @@ require "include/route.php";
   <!-- Load scripts -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="/assets/js/grid.js"></script>
   <script src="/assets/js/app.js"></script>
 
   <!-- Page footer -->
