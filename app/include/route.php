0b8582973cdd5a3e61faeaa43282d3188fdff90f
Arsalan Savand <arsalan@savandbros.com>
2017-08-26 04:23:19 +0400

    Add SSD VPS Hosting page to route

--- app/include/route.php
+++ app/include/route.php
@@ -138,6 +138,7 @@ Route::add("refund", "/legal/refunds", "Refund Policy", "Legal");
 Route::add("mybb", "/hosting/mybb", "MyBB", "Hosting");
 Route::add("mariadb", "/hosting/mariadb", "MariaDB", "Hosting");
 Route::add("bbpress", "/hosting/bbpress", "bbPress", "Hosting");
+Route::add("ssd-vps", "/hosting/ssd-vps", "SSD VPS", "Hosting");
 
 // Default route
 $base = Route::find("home");
