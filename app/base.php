ba28159b661a08e7dfcd8dd165d137cbf415caaf
Amir Savand <amir@savandbros.com>
2017-09-06 23:49:39 +0400

    CDN link of Font Awesome

--- app/base.php
+++ app/base.php
@@ -16,7 +16,7 @@ require "include/route.php";
 
   <!-- Load stylesheets -->
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="//fontawesome.io/assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="/assets/css/shorties.css">
   <link rel="stylesheet" href="/assets/css/app.css">
 </head>
