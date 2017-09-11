ed464370d18fbe3c59ab12e0e164ef40c4dc28d4
Amir Savand <amir@savandbros.com>
2017-09-11 14:50:33 +0400

    Bold kloud51 on home

--- app/pages/home.php
+++ app/pages/home.php
@@ -9,8 +9,8 @@ $plans = $db->select("SELECT * FROM `plans`");
 <!-- Cover -->
 <div class="container-fluid cover bg-header">
   <div class="container">
-    <h1 class="t-5">Kloud51</h1>
-    <h1>Web Hosting, Blazing Fast SSD VPS, Domain Names and SSL Certificate.</h1>
+    <h1 class="t-5"><b>Kloud51</b></h1>
+    <h1>Web Hosting, Domain Names and SSL Certificate.</h1>
   </div>
 </div>
 
