ba188d1f95864dfa3fa141edc9dbe6d8e13a0f82
Amir Savand <amir@savandbros.com>
2017-10-09 22:58:17 +0400

    Update tables

--- setup.sql
+++ setup.sql
@@ -4,19 +4,16 @@ CREATE TABLE `plans` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `title` varchar(50) NOT NULL,
  `subtitle` varchar(200) NOT NULL,
- `price` int(11) NOT NULL,
+ `price` float NOT NULL,
+ `price_renew` float NOT NULL,
  `featured` tinyint(1) NOT NULL DEFAULT '0',
+ `message` varchar(200) NOT NULL,
  `detail` text NOT NULL,
+ `tooltip` varchar(500) NOT NULL,
  `color` varchar(50) NOT NULL,
  `button_label` varchar(50) NOT NULL DEFAULT 'Order Now',
  `button_link` varchar(1000) NOT NULL DEFAULT '#',
- `hidden` BOOLEAN NOT NULL DEFAULT FALSE,
+ `type` varchar(20) NOT NULL DEFAULT 'hosting',
+ `hidden` tinyint(1) NOT NULL DEFAULT '0',
  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
-
-CREATE TABLE `plan_features` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `content` varchar(100) NOT NULL,
- `highlight` BOOLEAN NOT NULL DEFAULT FALSE,
- PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1
