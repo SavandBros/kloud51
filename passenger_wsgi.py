4f763c71b709451cb86aaa8704ebd95147f75076
Alireza Savand <alireza.savand@gmail.com>
2017-11-26 02:29:29 +0400

    Add WSGI deplouyment files

---
+++ passenger_wsgi.py
@@ -0,0 +1,12 @@
+"""
+WSGI config for Kloud51 project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloud51.settings")
+
+application = get_wsgi_application()
