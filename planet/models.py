68a2ec11f833d3714ecd2960ff7f64c6abc65e0e
Alireza Savand <alireza.savand@gmail.com>
2017-11-26 22:47:41 +0400

    Rough intialization of Planet model

---
+++ planet/models.py
@@ -0,0 +1,82 @@
+from currencies import Currency
+from django.db import models
+from planet import conf
+from django.utils.translation import ugettext_lazy as _
+
+
+class MoneyCurrency(models.Model):
+    """Money Currency."""
+    currency = models.CharField(
+        verbose_name=_("currency"),
+        max_length=113,
+        null=True,
+        blank=True,
+        choices=((k, k) for k in Currency.get_currency_formats())
+    )
+
+    def get_currency_parser(self):
+        """Return Money Currency parser."""
+        return Currency(self.currency)
+
+    class Meta:
+        verbose_name = _('Money Currency')
+        verbose_name_plural = _('Money Currencies')
+
+
+class Price(models.Model):
+    """Price model."""
+    PRICING_TYPE_CHOICES = (
+        (conf.PricingType.HOURLY, _('Hourly')),
+        (conf.PricingType.MONTHLY, _('Monthly')),
+        (conf.PricingType.QUARTERLY, _('Quarterly')),
+        (conf.PricingType.SEMI_ANNUALLY, _('Semi-Annually')),
+        (conf.PricingType.ANNUALLY, _('Annually')),
+        (conf.PricingType.BIENNIALLY, _('Biennially')),
+        (conf.PricingType.TRIENNIALLY, _('Triennially')),
+    )
+    currency = models.ManyToManyField(MoneyCurrency)
+    pricing_type = models.IntegerField(
+        verbose_name=_('pricing type'),
+        choices=PRICING_TYPE_CHOICES,
+    )
+    price = models.DecimalField(
+        verbose_name=_('price'),
+        decimal_places=3,
+        max_digits=10,
+    )
+
+    class Meta:
+        verbose_name = _('Price')
+        verbose_name_plural = _('Prices')
+
+
+class ProductGroup(models.Model):
+    """Product Group model."""
+    name = models.CharField(verbose_name=_('name'), max_length=50)
+    slug = models.SlugField(verbose_name=_('slug'), unique=True)
+    description = models.TextField(verbose_name='description')
+
+    class Meta:
+        verbose_name = _('Product Group')
+        verbose_name_plural = _('Product Groups')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """Product model."""
+    name = models.CharField(verbose_name=_('name'), max_length=50)
+    slug = models.SlugField(verbose_name=_('slug'), unique=True)
+    description = models.TextField(verbose_name='description')
+    featured = models.BooleanField(verbose_name=_('featured'))
+    group = models.ForeignKey(ProductGroup)
+    price = models.ManyToManyField(Price)
+
+    class Meta:
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+
+    def __str__(self):
+        return self.name
+
