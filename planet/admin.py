3658fedb9e600f02657f5c6cd24925660f57fcfb
Alireza Savand <alireza.savand@gmail.com>
2017-11-26 22:53:27 +0400

    Initialize the admin module for planet

--- planet/admin.py
+++ planet/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from planet.models import MoneyCurrency, Price, ProductGroup, Product
 
+
+@admin.register(MoneyCurrency)
+class MoneyCurrencyAdmin(admin.ModelAdmin):
+    list_display = ('currency', )
+    search_fields = ('currency', )
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ('currency', 'pricing_type', 'price', )
+    list_filter = ('pricing_type', )
+    search_fields = ('currency__currency', )
+
+
+@admin.register(ProductGroup)
+class ProductGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', )
+    search_fields = ('name', 'description', )
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'featured', 'in_stock', )
+    list_filter = ('featured', 'in_stock', )
+    search_fields = ('name', 'description', )
