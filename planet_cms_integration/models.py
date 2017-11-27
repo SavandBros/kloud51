e12573455e177cbb2462c94e15e4ebc091452fc6
Alireza Savand <alireza.savand@gmail.com>
2017-11-27 16:30:38 +0400

    Initialize planet_cms_integration models
    Still needs more work

---
+++ planet_cms_integration/models.py
@@ -0,0 +1,24 @@
+from cms.models import CMSPlugin
+from django.db import models
+
+from planet.models import Product, ProductGroup
+
+
+class ProductPluginModel(CMSPlugin):
+    """Product Plugin Model."""
+    product = models.ForeignKey(Product)
+    description = models.TextField(verbose_name='description')
+    name = models.CharField(verbose_name=_('name'), max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductGroupPluginModel(CMSPlugin):
+    """Product Group Plugin Model."""
+    product_group = models.ForeignKey(ProductGroup)
+    name = models.CharField(verbose_name=_('name'), max_length=50)
+    description = models.TextField(verbose_name='description')
+
+    def __str__(self):
+        return self.name
