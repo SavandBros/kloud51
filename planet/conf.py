077edab9fd132ac08907b86245bbf59adddb6b52
Alireza Savand <alireza.savand@gmail.com>
2017-11-30 04:47:11 +0400

    Add SECTION_TEMPLATES to planet conf

--- planet/conf.py
+++ planet/conf.py
@@ -8,3 +8,10 @@ class PricingType:
     ANNUALLY = 6
     BIENNIALLY = 7
     TRIENNIALLY = 8
+
+
+SECTION_TEMPLATES = (
+    ('planet/cms/sections/feature_icon.html', 'Features with Icons'),
+    ('planet/cms/sections/feature_images_title.html',
+     'Feature with Images & and Title.'),
+)
