27433e3cb6fc5516223d18c48bb39bd4c808cf6a
Alireza Savand <alireza.savand@gmail.com>
2017-12-01 16:01:39 +0400

    Getting the custom fix for now

--- kloud51/settings.py
+++ kloud51/settings.py
@@ -205,7 +205,7 @@ if USE_S3:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=864000',
