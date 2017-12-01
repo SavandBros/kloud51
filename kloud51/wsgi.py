2f0a4a51cc1494bf3cff956c960e2648a8e32ca8
Alireza Savand <alireza.savand@gmail.com>
2017-12-01 14:54:10 +0400

    More weird stuff for passenger

--- kloud51/wsgi.py
+++ kloud51/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloud51.settings")
+from django.conf import settings
 
 application = get_wsgi_application()
+application = WhiteNoise(application, settings.STATIC_ROOT)
