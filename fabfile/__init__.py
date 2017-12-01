68afb12dbb2f90f3ba1ecec584eb3b558b8d3b52
Alireza Savand <alireza.savand@gmail.com>
2017-12-01 19:13:39 +0400

    Add fabfile

---
+++ fabfile/__init__.py
@@ -0,0 +1,3 @@
+from .fab_config import *  # noqa
+from .fab_assert import *  # noqa
+from .fab_development import *  # noqa
---
+++ fabfile/fab_config.py
@@ -0,0 +1,3 @@
+from typing import Tuple
+
+apps: Tuple[str] = ('planet', )
