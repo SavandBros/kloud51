097a0ead9151a07f40783256b1787029c9b94c1e
Alireza Savand <alireza.savand@gmail.com>
2017-12-02 16:36:20 +0400

    Add sub_title to SectionTranslationOptions

--- planet/translation.py
+++ planet/translation.py
@@ -29,7 +29,7 @@ class TeamMemberTranslationOptions(TranslationOptions):
 
 
 class SectionTranslationOptions(TranslationOptions):
-    fields: Tuple[str] = ('title', 'description', )
+    fields: Tuple[str] = ('title', 'sub_title', 'description', )
 
 
 class SectionItemTranslationOptions(TranslationOptions):
