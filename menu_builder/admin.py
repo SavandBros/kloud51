bf46a4fabc6dd1a50c3e47d73b4a3b9b6da0e1cd
Alireza Savand <alireza.savand@gmail.com>
2017-12-04 02:14:16 +0400

    Add menu builder admin modules

--- menu_builder/admin.py
+++ menu_builder/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from typing import Tuple, List
 
-# Register your models here.
+from adminsortable2.admin import SortableInlineAdminMixin
+from django.contrib import admin
+from modeltranslation.admin import TranslationTabularInline
+
+from menu_builder.forms import NavigationLinkModelForm
+from menu_builder.models import Menu, NavigationLink
+
+
+class NavigationLinkInline(SortableInlineAdminMixin, TranslationTabularInline):
+    """Navigation Link model Inline."""
+    model = NavigationLink
+    form = NavigationLinkModelForm
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    """Menu model admin."""
+    list_display: Tuple[str] = ('name', )
+    inlines: List[admin.TabularInline] = [
+        NavigationLinkInline,
+    ]
